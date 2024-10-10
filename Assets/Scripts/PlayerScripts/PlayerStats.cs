1dc9af0b0d6ea0eb07003636fdb87649561ab497
MateuszCzerniawski <128147019+MateuszCzerniawski@users.noreply.github.com>
2024-10-10 20:49:14 +0200

    damage types added to player stats

--- Assets/Scripts/PlayerScripts/PlayerStats.cs
+++ Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -8,11 +8,20 @@ public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float maxHp = 100f;
     [SerializeField] private TMP_Text txt;
+    [SerializeField] private float defaultMeleeDamage=30;
+    [SerializeField] private float defaultRangedDamage=20;
+    [SerializeField] private float defaultDashDamage=10;
     private float _currHP;
+    private float _meleeDamage;
+    private float _rangedDamage;
+    private float _dashDamage;
     
     private void Start()
     {
         _currHP = maxHp;
+        _meleeDamage = defaultMeleeDamage;
+        _rangedDamage = defaultRangedDamage;
+        _dashDamage = defaultDashDamage;
     }
 
     public void ChangeHp(float dmg)
