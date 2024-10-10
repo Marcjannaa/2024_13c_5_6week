0e928c991e0bf4f64a651e0ef925239248e8ca2e
s24666 <s27466@pjwstk.edu.pl>
2024-10-10 18:55:09 +0200

    dodano wszystkie platformy

---
+++ Assets/Scripts/Objects/MovingPlatform.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private string InitialDirection;
+    [SerializeField] private float MovingSpeed;
+    [SerializeField] private float MovingRange;
+    [SerializeField] private float MoveDelay;
+    private Vector3 CurrentPos;
+    private bool MovingRight;
+    void Start()
+    {
+        CurrentPos = transform.position;
+        MovingRight = InitialDirection.ToLower() == "right";
+        StartCoroutine(MoveCoroutine());
+    }
+
+    private IEnumerator MoveCoroutine()
+    {
+        while (true)
+        {
+            float targetX;
+            CurrentPos = transform.position;
+            
+            if (MovingRight)
+            {
+                targetX = CurrentPos.x + MovingRange;
+            }
+            else
+            {
+                targetX = CurrentPos.x - MovingRange;
+            }
+
+            Vector3 targetPos = new Vector3(targetX, transform.position.y, transform.position.z);
+            
+            while (Mathf.Abs(transform.position.x - targetX) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, MovingSpeed * Time.deltaTime);
+                yield return null;
+            }
+            
+            yield return new WaitForSeconds(MoveDelay);
+            
+            if (MovingRight)
+            {
+                MovingRight = false;
+            }
+            else
+            {
+                MovingRight = true;
+            }
+        }
+    }
+}
