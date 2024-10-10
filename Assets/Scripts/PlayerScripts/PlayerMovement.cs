f4be4248ced0b0e6ec7ca91536ed327b496678d4
MateuszCzerniawski <128147019+MateuszCzerniawski@users.noreply.github.com>
2024-10-10 20:59:26 +0200

    weird error with commit? Double jump recommited

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -14,25 +14,47 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpForce = 5f;
 
     private bool _canJump = true;
+    private bool _canDoubleJump = false;
 
     private void OnTriggerStay2D (Collider2D other)
     {
         if (other.CompareTag("Floor"))
+        {
             _canJump = true;
+            _canDoubleJump = false;
+        }
     }
 
     private void Update() 
     {
-        if (Input.GetKey(KeyCode.A)) 
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+        }
         
         else if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-
-        if (Input.GetKeyDown(KeyCode.Space) && _canJump)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            _canJump = false;
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(_canJump){
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                _canJump = false;
+                _canDoubleJump = true;
+            }
+            else if (_canDoubleJump)
+            {
+                float forceY = (rb.totalForce.y * rb.mass * -1) + jumpForce;
+                rb.AddForce(new Vector2(0, forceY), ForceMode2D.Impulse);
+                _canDoubleJump = false;
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 }
