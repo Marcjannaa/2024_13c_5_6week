6e4dc88fe8b556bf629b8f09efe89e6bebf3f5f7
Marcjannaa <32717296+Marcjannaa@users.noreply.github.com>
2024-10-24 22:36:06 +0200

    player regular attacks up/down/left/right

--- Assets/Scripts/PlayerScripts/PlayerMeleeAttack.cs
+++ Assets/Scripts/PlayerScripts/PlayerMeleeAttack.cs
@@ -1,20 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using PlayerScripts;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 
 public class PlayerMeleeAttack : MonoBehaviour
 {
     [SerializeField] private Transform _transform;
     [SerializeField] private LayerMask attackableLayer;
-    [SerializeField] private float attackRange = 5f;
+    [SerializeField] private float attackRange = 0.3f;
     private bool _shouldDamage;
     private RaycastHit2D[] hits;
+    Vector2 t;
+    private string key;
     
     public void DealDamage()
     {
         hits = Physics2D.CircleCastAll(transform.position, attackRange, transform.right, 0f, attackableLayer);
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            hits = Physics2D.CircleCastAll(transform.position, attackRange, transform.up * Input.GetAxis("Vertical"), 1f, attackableLayer);
+        }else if (Input.GetAxis("Horizontal") > 0)
+        {
+            hits = Physics2D.CircleCastAll(transform.position, attackRange, transform.right * Input.GetAxis("Horizontal"), 1f, attackableLayer);
+        }
+        
         foreach (var t in hits)
         {
             var idamageable = t.collider.gameObject.GetComponent<IDamageable>();
@@ -28,7 +42,6 @@ public class PlayerMeleeAttack : MonoBehaviour
             idamageable.Damage(
                 gameObject.GetComponent<PlayerStats>().GetMeleeDamage(), t.collider.gameObject
             );
-            
             print("hiiiit1!!");
         }
     }
