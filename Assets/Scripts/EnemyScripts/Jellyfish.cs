0037751dd9d2383e9e446cf94012d8f8506a4ea6
s24666 <s27466@pjwstk.edu.pl>
2024-10-26 19:17:33 +0200

    meduza wisi

--- Assets/Scripts/EnemyScripts/Jellyfish.cs
+++ Assets/Scripts/EnemyScripts/Jellyfish.cs
@@ -1,23 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class Jellyfish : Enemy
 {
-    // Start is called before the first frame update
+    [SerializeField]private List<Vector2> positions = new List<Vector2>() { new(0f, 0f), new(0f, 0f), new(0f, 0f) };
+    private float atkDelay;
+    [SerializeField] private float speed;
+    private float dmgDealy = 0.5f;
+    private float counter = 0f;
+    private float dmg = 30f;
+    
+    private enum JellyAttack
+    {
+        Sweep,
+        Drain,
+        Shoot,
+        Move
+    }
+    
     void Start()
     {
-        
+        Hp = MaxHp;
     }
 
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) { return; }
+        
+
+    }
+    
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")){return;}
+        if (counter <= 0f)
+        {
+            Attack(other.gameObject);
+            counter = dmgDealy;
+        }else counter -= Time.deltaTime;
+    }
     protected override void Attack(GameObject go)
     {
-        throw new System.NotImplementedException();
+        go.GetComponent<PlayerStats>().DealDamage(dmg);
     }
 
     public override void ChangeHp(float damage)
     {
-        throw new System.NotImplementedException();
+        if (Hp - damage <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Hp -= damage;
+//        gameObject.GetComponent<HpBar>().UpdateBar(Hp, MaxHp);
     }
 
     // Update is called once per frame
@@ -25,4 +63,12 @@ public class Jellyfish : Enemy
     {
         
     }
+
+    void movingAttack(GameObject go)
+    {
+        var multi = go.gameObject.transform.position.x > transform.position.x ? 1 : -1;
+        transform.position += new Vector3(multi * speed * Time.deltaTime, 0, 0);
+    }
 }
+
+
