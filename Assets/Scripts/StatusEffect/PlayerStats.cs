1917a71c0bafd1a1c88e0a1d6114c9f013907ed7
MateuszCzerniawski <128147019+MateuszCzerniawski@users.noreply.github.com>
2024-11-06 19:02:03 +0100

    player default data loading improved
    Added new script which is responsible for loading player data if missing. More work on saving and loading data required, this new sript fixes only one problem, but more problems related to saves may occur

--- Assets/Scripts/StatusEffect/PlayerStats.cs
+++ Assets/Scripts/StatusEffect/PlayerStats.cs
@@ -28,8 +28,8 @@ public class PlayerStats : MonoBehaviour
     private void Start()
     {
         _heartCount = hearts.Count;
-        _currentHp = maxHp;//_currentHp = PlayerPrefs.GetFloat("MaxHp");
-        meleeDamage = defaultMeleeDamage;//meleeDamage = PlayerPrefs.GetFloat("MeleeAttack");
+        _currentHp = PlayerPrefs.GetFloat("MaxHp");//_currentHp = maxHp;
+        meleeDamage = PlayerPrefs.GetFloat("MeleeAttack");//meleeDamage = defaultMeleeDamage;
         _rangedDamage = defaultRangedDamage;
         _dashDamage = defaultDashDamage;
 
@@ -47,7 +47,7 @@ public class PlayerStats : MonoBehaviour
     }
     private void ResetHp()
     {
-        _currentHp = maxHp;//_currentHp = PlayerPrefs.GetFloat("MaxHp");
+        _currentHp = PlayerPrefs.GetFloat("MaxHp");//_currentHp = maxHp;
     }
 
     public void DealDamage(float dmg)
